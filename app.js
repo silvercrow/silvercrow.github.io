8b85fd71187babcf452309becd5acbd94e4c94d5
David Balan <sc@silvercrow.org>
2020-01-07 22:17:06 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 
 
 function handleLedClick(cb) {
+     Puck.eval("BTN.read()",function(x) { console.log(x); })
     if(cb.checked){
-     Puck.write('require("https://github.com/silvercrow/espruino-sandbox/blob/master/app.js");\n');
+     //Puck.write('require("https://github.com/silvercrow/espruino-sandbox/blob/master/app.js");\n');
      Puck.write('analogWrite(13,1);\n');
     }else{
      Puck.write('analogWrite(13,0);\n');
